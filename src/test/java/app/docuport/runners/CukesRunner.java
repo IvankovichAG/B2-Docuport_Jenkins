7ace738349bbd242eab404b6299932e0f28fb5c4
IvankovichAG <85silean@gmail.com>
2024-02-25 01:53:34 -0500

    messageone

--- src/test/java/app/docuport/runners/CukesRunner.java
+++ src/test/java/app/docuport/runners/CukesRunner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
-                  "html:target/default-html-reports"},
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml"},
         features = "src/test/resources/features/",
         glue = "app/docuport/step_definitions/",
+       // publish = true,
         tags = "@ui",
         dryRun = false
 )
